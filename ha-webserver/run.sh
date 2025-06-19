349d2a7c4b5605249ad30ba29e615af7131df4fa
Joel <63735432+JOHLC@users.noreply.github.com>
2025-06-19 14:31:14 -0400

    Update run.sh

--- ha-webserver/run.sh
+++ ha-webserver/run.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-# Serve HTML files for Home Assistant Webserver Addon
+# Serve files from addon_config (/config)
 
-WEB_ROOT="/data"
-[ ! -d "$WEB_ROOT" ] && WEB_ROOT="/config/www"
-
+WEB_ROOT="/config"
 PORT=${INGRESS_PORT:-8080}
 
-cd "$WEB_ROOT"
+cd "$WEB_ROOT" || exit 1
 exec python3 -m http.server "$PORT" --bind 0.0.0.0
